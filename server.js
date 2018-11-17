34d2930f16c105bb969258a0fc85faf6158ab438
ajinfadhila <ajinfadhila@gmail.com>
2018-11-17 20:26:38 +0700

    up again

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ var app = express();
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static('public'));
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
