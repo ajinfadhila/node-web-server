e492a50b376f5118d3cc7e3a9833f806e15edab6
ajinfadhila <ajinfadhila@gmail.com>
2018-11-17 20:50:47 +0700

    up again

--- server.js
+++ server.js
@@ -4,15 +4,12 @@ const fs = require('fs');
 
 const port = process.env.PORT || 3000;
 var app = express();
+
 var http = require('http');
 var server = http.Server(app);
 
-app.use(express.static('public'));
+app.use(express.static('client'));
 
-// server.listen(PORT, function() {
-//   console.log('Chat server running');
-// });
-
-server.listen(port, () => {
-  console.log(`Server is up on port ${port}`);
+server.listen(port, function() {
+  console.log('Chat server running');
 });
