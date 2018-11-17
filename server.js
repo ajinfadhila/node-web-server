b6e3918fc23c13dd7de71895a82dbcae181ac913
ajinfadhila <ajinfadhila@gmail.com>
2018-11-17 23:30:03 +0700

    kenapa ga bisa

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var app = express();
 var http = require('http');
 var server = http.Server(app);
 
-// hbs.registerPartials(__dirname + '/views/partials')
-// app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
 //
 // app.use((req, res, next) => {
 //   var now = new Date().toString();
