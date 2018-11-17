606fcb6bdb1f5406f1cfb624510e258e44795a67
ajinfadhila <ajinfadhila@gmail.com>
2018-11-17 23:41:28 +0700

    kenapa ga bisa

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ app.set('view engine', 'hbs');
 // app.use((req, res, next) => {
 //   res.render('maintenance.hbs');
 // });
-app.use(express.static('client'));
+// app.use(express.static('client'));
 app.use(express.static(__dirname + '/public'));
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
