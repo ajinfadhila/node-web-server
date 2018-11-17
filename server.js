5d14201f54281a92b6ec308ab03cb75596b50d2c
ajinfadhila <ajinfadhila@gmail.com>
2018-11-17 22:47:39 +0700

    coba ah

--- server.js
+++ server.js
@@ -6,8 +6,24 @@ const port = process.env.PORT || 3000;
 var app = express();
 var http = require('http');
 var server = http.Server(app);
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
 app.use(express.static('client'));
 
-server.listen(port, function() {
+server.listen(PORT, function() {
   console.log('Chat server running');
 });
